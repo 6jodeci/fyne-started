cbacf421040d529b45a8395f78328d45de29592b
6jodeci <egor0263s@gmail.ru>
2022-06-03 12:15:08 +0500

    1

---
+++ positionContainer/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+)
+
+func main() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Container")
+	green := color.NRGBA{0, 180, 0, 255}
+
+	text1 := canvas.NewText("Hello", green)
+	text2 := canvas.NewText("There", green)
+	text2.Move(fyne.NewPos(20, 20))
+	content := container.NewWithoutLayout(text1, text2)
+
+	myWindow.SetContent(content)
+	myWindow.ShowAndRun()
+}
